f5ddb9cf2500826e2cf5b8a89b996fde3e2bf8ae
Jan Weidner <jw3126@gmail.com>
2020-10-14 18:51:21 +0200

    add prolong docs

--- README.md
+++ README.md
@@ -33,4 +33,20 @@ julia> range(start=around(0.9), stop=3.0, step=1)
 
 julia> range(start=strictbelow(1.0), stop=3.0, step=1)
 0.0:1.0:3.0
+
+julia> r = 1.0:0.5:3.0
+1.0:0.5:3.0
+
+julia> prolong(r, stop=around(4))
+1.0:0.5:4.0
+
+julia> prolong(r, pre=1)
+0.5:0.5:3.0
+
+julia> prolong(r, pre=1, post=2)
+0.5:0.5:4.0
+
+julia> prolong(r, start=below(0.4), stop=around(4.1))
+0.0:0.5:4.0
+```
 ```
--- src/RangeHelpers.jl
+++ src/RangeHelpers.jl
@@ -191,6 +191,30 @@ end
 ################################################################################
 ##### prolong
 ################################################################################
+"""
+    prolong(r::AbstractRange;start=nothing, stop=nothing, pre=nothing, post=nothing)
+
+Prolong an existing range `r` according to the arguments:
+
+```jldoctest
+julia> using RangeHelpers
+
+julia> r = 1.0:0.5:3.0
+1.0:0.5:3.0
+
+julia> prolong(r, stop=around(4))
+1.0:0.5:4.0
+
+julia> prolong(r, pre=1)
+0.5:0.5:3.0
+
+julia> prolong(r, pre=1, post=2)
+0.5:0.5:4.0
+
+julia> prolong(r, start=below(0.4), stop=around(4.1))
+0.0:0.5:4.0
+```
+"""
 function prolong(r::AbstractRange;start=nothing, stop=nothing, pre=nothing, post=nothing)
     r1 = prolong_start(r, start)
     r2 = prolong_stop(r1, stop)
